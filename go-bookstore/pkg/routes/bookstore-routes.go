9f5e68d7b213a677a1521ceaf9b680f2aadcae10
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2026-01-11 15:33:56 +0900

    go-bookstore: book-controller.go

--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -6,8 +6,8 @@ import (
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
-	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/books/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/books/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/books/", controllers.GetBooks).Methods("GET")
 	router.HandleFunc("/books/{bookId}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/books/{bookId}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/books/{bookId}", controllers.DeleteBook).Methods("DELETE")

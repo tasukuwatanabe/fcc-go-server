d7a794ddf811dbfedbb1ad3ec743f0b48428d308
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2026-01-12 17:53:16 +0900

    go-bookstore: fix

--- go-bookstore/pkg/config/app.go
+++ go-bookstore/pkg/config/app.go
@@ -5,9 +5,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-var (
-	db *gorm.DB
-)
+var db *gorm.DB
 
 func Connect() {
 	d, err := gorm.Open("mysql", "admin:password@/simplerest?charset=utf8&parseTime=True&loc=Local")
